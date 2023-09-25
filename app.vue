cda26387fd1fcbf3b042ad65e423f45f88271913
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-26 05:04:36 +0600

    4.1_components directory(a)

--- app.vue
+++ app.vue
@@ -2,49 +2,8 @@
   <div>
    Hello Sohan
   </div>
-  <button @click="increment">Increment</button>
-  <p>{{count}}</p>
-<button @click="decrement">Decrement</button>
+ <Counter />
 </template>
-<!-- 
-
-  <script setup>
-const count = ref(0);
-
-function increment(){
- count.value++;
-
-}
-
-function decrement(){
-  count.value--;
 
 
-}
-
-</script>
- -->
-<script>
-export default {
-  data(){
-    return {
-       count: 0,
-    };
-
-  },
-  methods: {
-    increment(){
-      this.count++;
-
-    },
-    decrement(){
-      this.count--;
-
-    },
-
-  },
-
-
-};
-
-</script>
+ 
