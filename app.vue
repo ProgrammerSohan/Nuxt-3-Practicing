3b5e7ef14cdbbceabed0276ac1221a3657f34fec
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-25 22:53:21 +0600

    2_auto import_composition API(a)

--- app.vue
+++ app.vue
@@ -2,4 +2,22 @@
   <div>
    Hello Sohan
   </div>
+  <button @click="increment">Increment</button>
+  <p>{{count}}</p>
+<button @click="decrement">Decrement</button>
 </template>
+<script setup>
+const count = ref(0);
+
+function increment(){
+ count.value++;
+
+}
+
+function decrement(){
+  count.value--;
+
+
+}
+
+</script>
