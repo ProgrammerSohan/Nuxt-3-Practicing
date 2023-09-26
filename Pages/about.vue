c91a2e743551fe8bfa5f2377bb011f183335fa89
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-27 02:45:05 +0600

    7.1_page navigation

--- Pages/about.vue
+++ Pages/about.vue
@@ -1,3 +1,7 @@
 <template>
+<div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="about">About</NuxtLink>
     This is about page.
+</div>
 </template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,3 +1,16 @@
 <template>
+   <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="about">About</NuxtLink>
+    
+    <button @click="moveToAbout">Take me to about page</button>
     Hello Sohan! Index Page.
+   </div>
 </template>
+<script setup>
+    function moveToAbout(){
+
+        navigateTo("about");
+    }
+
+</script>
